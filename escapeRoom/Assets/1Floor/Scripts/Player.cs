86433daea6a864eecee69045b18cf50764fbd32b
InSange <ehfrur888@naver.com>
2022-05-21 04:30:25 +0900

    플레이어 발걸음, 속도, 점프 수정 및 체력UI 제작

--- escapeRoom/Assets/1Floor/Scripts/Player.cs
+++ escapeRoom/Assets/1Floor/Scripts/Player.cs
@@ -5,58 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    /*
-    public Rigidbody playerRigidbody;
-    public Camera fpsCam;
-
-    float MoveSpeed = 5.0f;
-    float rotSpeed = 3.0f;
-    float currentRot = 0f;
-
-    // Update is called once per frame
-    void Update()
+    /* // 싱글톤 // * instance라는 변수를 static으로 선언을 하여 다른 오브젝트 안의 스크립트에서도 instance를 불러올 수 있게 합니다 */
+    public static Player instance = null; 
+    private void Awake()
     {
-        Move();
-        RotCtrl();
+        if (instance == null) //instance가 null. 즉, 시스템상에 존재하고 있지 않을때 
+        { 
+            instance = this; //내자신을 instance로 넣어줍니다.
+            DontDestroyOnLoad(gameObject); //OnLoad(씬이 로드 되었을때) 자신을 파괴하지 않고 유지 
+        } 
+        else 
+        { 
+            if (instance != this) //instance가 내가 아니라면 이미 instance가 하나 존재하고 있다는 의미 
+                Destroy(this.gameObject); //둘 이상 존재하면 안되는 객체이니 방금 AWake된 자신을 삭제 
+        } 
     }
 
-    void Move()
-    {
-        float xInput = Input.GetAxis("Horizontal");
-        float zInput = Input.GetAxis("Vertical");
-
-        float xSpeed = xInput * MoveSpeed;
-        float zSpeed = zInput * MoveSpeed;
-
-        transform.Translate(Vector3.forward * zSpeed * Time.deltaTime);
-        transform.Translate(Vector3.right * xSpeed * Time.deltaTime);
-    }
-
-    void RotCtrl()
-    {
-        float rotX = Input.GetAxis("Mouse Y") * rotSpeed;
-        float rotY = Input.GetAxis("Mouse X") * rotSpeed;
-
-        // 마우스 반전
-        currentRot -= rotX;
-
-        // 마우스가 특정 각도를 넘어가지 않게 예외처리
-        currentRot = Mathf.Clamp(currentRot, -80f, 80f);
-
-        // Camera는 Player의 자식이므로 플레이어의 Y축 회전은 Camera에게도 똑같이 적용됨
-        this.transform.localRotation *= Quaternion.Euler(0, rotY, 0);
-        // Camera의 transform 컴포넌트의 로컬로테이션의 오일러각에 
-        // 현재X축 로테이션을 나타내는 오일러각을 할당해준다.
-        fpsCam.transform.localEulerAngles = new Vector3(currentRot, 0f, 0f);
-    }
-    */
-    
     public CharacterController SelectPlayer; // 제어할 캐릭터 컨트롤러
     public Camera fpsCam;
     public float Speed = 5.0f;  // 이동속도
     public float JumpPow = 5.0f;
 
-    private float Gravity = 10.0f; // 중력   
+    private float Gravity = 20.0f; // 중력   
     private Vector3 MoveDir = Vector3.zero; // 캐릭터의 움직이는 방향.
     private bool JumpButtonPressed = false;  //  최종 점프 버튼 눌림 상태
 
@@ -64,8 +34,10 @@ public class Player : MonoBehaviour
     float currentRot = 0f;
 
     public AudioClip footStepSound;
-    public float footStepDelay;
+    public float footStepDelay = 0.5f;
     private float nextFootstep = 0;
+    public bool slowStep = false;
+    public GameObject[] life = { };
 
     // Update is called once per frame
     void Update()
@@ -104,6 +76,18 @@ public class Player : MonoBehaviour
             // 오브젝트가 바라보는 앞방향으로 이동방향을 돌려서 조정합니다.
             MoveDir = SelectPlayer.transform.TransformDirection(MoveDir);
             // 속도를 곱해서 적용합니다.
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                GetComponent<AudioSource>().volume = 0.35f;
+                footStepDelay = 1.0f;
+                Speed = 2.0f;
+            }
+            else
+            {
+                GetComponent<AudioSource>().volume = 1.0f;
+                footStepDelay = 0.5f;
+                Speed = 5.0f;
+            }
             MoveDir *= Speed;
 
             // 스페이스 버튼에 따른 점프 : 최종 점프버튼이 눌려있지 않았던 경우만 작동
